b3c76535daa2e63bb2641fb911856d13b5211722
jbauer12 <jonasbauerwi@gmail.com>
2023-12-17 10:00:34 +0100

    Minimax Gerüst fertig.

--- Go/minimax/minimax.go
+++ Go/minimax/minimax.go
@@ -2,10 +2,16 @@ package minimax
 
 import (
 	"checkers/packages/gameboard"
+	"checkers/packages/possible_moves"
 	"fmt"
 
 	"github.com/samber/lo"
 )
+
+type BestScore struct {
+	Action gameboard.Action
+	Score  float64
+}
 
 func opposite(team string, smash bool) string {
 	if smash {
@@ -65,4 +71,41 @@ func ValueFrom(gameBoard gameboard.GameBoard) float64 {
 	opponentScore := reduce_function(filter_function(flattenBoard, opposite(gameBoard.CurrPlayer, false)))
 
 	return opponentScore - playerScore
+}
+func Player(gameBoard gameboard.GameBoard) string {
+	return gameBoard.CurrPlayer
+}
+func Actions(gameBoard gameboard.GameBoard, team string) []gameboard.Action {
+	possible_moves := possible_moves.Get_all_possible_moves_for_team(gameBoard, team)
+	return possible_moves
+}
+func Result(gameBoard gameboard.GameBoard, action gameboard.Action) gameboard.GameBoard {
+	if !Terminal(gameBoard) {
+		return possible_moves.Make_move(gameBoard, action)
+	}
+	return gameBoard
+}
+func Minimax(state gameboard.GameBoard, depth int, player string) BestScore {
+
+	bestScore := BestScore{gameboard.Action{}, -1000000}
+	if player == "G" {
+		bestScore.Score = 1000000
+	}
+
+	if depth == 0 || Terminal(state) {
+		score := ValueFrom(state)
+		return BestScore{gameboard.Action{}, score}
+	}
+
+	for _, action := range Actions(state, player) {
+		result1 := Result(state, action)
+		value := Minimax(result1, depth-1, opposite(player, result1.CurrPlayer == player))
+
+		if player == "R" && value.Score > bestScore.Score {
+			bestScore = BestScore{action, value.Score}
+		} else if player == "G" && value.Score < bestScore.Score {
+			bestScore = BestScore{action, value.Score}
+		}
+	}
+	return bestScore
 }
---
+++ Go/possible_moves/possible_moves.go
@@ -0,0 +1,13 @@
+package possible_moves
+
+import (
+	"checkers/packages/gameboard"
+)
+
+func Get_all_possible_moves_for_team(gameBoard gameboard.GameBoard, currPlayer string) []gameboard.Action {
+	return []gameboard.Action{}
+}
+
+func Make_move(gameBoard gameboard.GameBoard, action gameboard.Action) gameboard.GameBoard {
+	return gameBoard
+}
